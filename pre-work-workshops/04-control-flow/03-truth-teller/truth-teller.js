987a29cbf6aaa4c4e107fb9095143e1acdf11119
ShankarBirTamang <sankar.jt19@gmail.com>
2025-03-25 19:16:53 +0545

    truth teller exercises done

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -1,4 +1,4 @@
-let boolean1 = true, boolean2 = true;
+let boolean1 = false, boolean2 = true;
 // let boolean1 = true, boolean2 = false;
 // let boolean1 = false, boolean2 = true;
 // let boolean1 = false, boolean2 = false;
@@ -6,3 +6,10 @@ let boolean1 = true, boolean2 = true;
 let result;
 
 // YOUR CODE BELOW
+
+result = boolean1===true && boolean2 ===true ? 'both': 
+    boolean1===true && boolean2 === false || boolean1 === false && boolean2 === true ? 'one': 'none';
+
+    console.log(result);
+
+   
