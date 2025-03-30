f906db77c9ec87b38826ff82645938b2027ff595
ShankarBirTamang <sankar.jt19@gmail.com>
2025-03-30 21:19:32 +0545

    array-ii ...

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function arrayFlattener(twoDArray) {
+  let flatArray = [];
+  for (let i = 0; i < twoDArray.length; i++) {
+    let element = twoDArray[i];
+    if (Array.isArray(element)) {
+      for (let j = 0; j < element.length; j++) {
+        let innerElement = element[j];
+        flatArray.push(innerElement);
+      }
+    } else {
+      flatArray.push(element);
+    }
+  }
+  return flatArray;
+}
+console.log(arrayFlattener([1, [2, 3], 4]));
--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function zooInventory(myZoo) {
+  let flatArray = [];
+  for (let i = 0; i < myZoo.length; i++) {
+    let animal = myZoo[i];
+    let name = animal[0];
+    let species = animal[1][0];
+    let age = animal[1][1];
+    let sentence = `${name} the ${species} is ${age}.`;
+    flatArray.push(sentence);
+  }
+  return flatArray;
+}
+
+let zoo = [
+  ["King Kong", ["gorilla", 42]],
+  ["Nemo", ["fish", 5]],
+  ["Punxsutawney Phil", ["groundhog", 11]],
+];
+
+console.log(zooInventory(zoo));
