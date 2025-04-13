be9a5c11ffb14f8ff16c0425c493a27a6061a0c7
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-04-13 19:49:29 +0545

    Day1 degugging-basics...

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -1,1 +1,23 @@
 /* eslint-disable no-unused-vars */
+const setPropsOnObj = (obj) => {
+  obj.x = 7;
+  obj["y"] = 8;
+  obj.onePlus = (num) => num + 1;
+};
+
+const setPropsOnArr = (arrObj) => {
+  arrObj.hello = () => "Hello!";
+  arrObj["full"] = "stack";
+  arrObj[0] = 5;
+  arrObj.twoTimes = (num) => num * 2;
+};
+
+const setPropsOnFunc = (functionObject) => {
+  functionObject.year = "20??";
+  functionObject.divideByTwo = (num) => num / 2;
+};
+
+const shallowCopy = (firstArray, secondArray) => {
+  if (Array.isArray(firstArray)) return [...firstArray, ...secondArray];
+  return { ...firstArray, ...secondArray };
+};
