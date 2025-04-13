95422c432b1d583b6d4eb2f22357ce2860fc5b60
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-04-13 20:21:43 +0545

    object properties...

--- tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
+++ tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-unused-vars */
-let dog = {};
 
-dog.bark = () => false;
-dog.name = "Cody";
+const name = "Cody";
+const loud = "loud";
+
+const dog = {
+  bark() {
+    return "ruff ruff!";
+  },
+  name,
+  [`${loud}Bark`]: () => dog.bark().toUpperCase(),
+};
+
+const { bark } = dog;
