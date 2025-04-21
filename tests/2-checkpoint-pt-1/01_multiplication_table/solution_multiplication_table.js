2a80e86ef715266e00b02667978efa6f4a76b47b
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-04-21 14:12:12 +0545

    multiplication table completed...

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+function multiplicationTable(rows, cols) {
+  let table = [];
+  for (let i = 1; i <= rows; i++) {
+    let rowArr = [];
+    for (let j = 1; j <= cols; j++) {
+      rowArr.push(i * j);
+    }
+    table.push(rowArr);
+  }
+  return table;
+}
+
+const smallTable = multiplicationTable(3, 0);
+const mediumTable = multiplicationTable(6, 0);
+const largeTable = multiplicationTable(9, 0);
+
+console.log(smallTable);
+console.log(mediumTable);
+console.log(largeTable);
+
+console.log(multiplicationTable(2, 3));
