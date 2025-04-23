7218b709cd8bde75f0c3bb323275746570c36fdd
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-04-23 18:14:26 +0545

    test3: simulate battle complete

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,1 +1,47 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+//constructor function
+
+function Pokemon(name, health, attackBonus) {
+  this.name = name;
+  this.health = health;
+  this.attackBonus = attackBonus;
+}
+
+Pokemon.prototype.biteAttack = function () {
+  return this.attackBonus + 2;
+};
+
+Pokemon.prototype.isDefeated = function () {
+  return this.health <= 0;
+};
+
+pikachu = new Pokemon("Pikachu", 250, 4);
+charizard = new Pokemon("Charizard", 300, 5);
+
+console.log(pikachu.name);
+console.log(pikachu.health);
+console.log(pikachu.attackBonus);
+console.log(pikachu.biteAttack());
+console.log(pikachu.isDefeated());
+
+function simulateBattle(pokemon1, pokemon2, firstAttacker) {
+  let attacker, defender;
+
+  //Determine initial attacker and defender
+  if (pokemon1.name === firstAttacker) {
+    attacker = pokemon1;
+    defender = pokemon2;
+  } else if (pokemon2.name === firstAttacker) {
+    attacker = pokemon2;
+    defender = pokemon1;
+  }
+
+  //Battle loop
+  while (true) {
+    defender.health -= attacker.biteAttack();
+    if (defender.isDefeated()) {
+      return `${attacker.name} Wins!`;
+    }
+    [attacker, defender] = [defender, attacker];
+  }
+}
