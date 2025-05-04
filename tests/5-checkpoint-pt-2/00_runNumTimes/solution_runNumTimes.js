d393c1dfc008ce576fa4ce44561bc788e4a5a704
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-04 21:31:37 +0545

    test 5.0 : runNumTimes done

--- tests/5-checkpoint-pt-2/00_runNumTimes/solution_runNumTimes.js
+++ tests/5-checkpoint-pt-2/00_runNumTimes/solution_runNumTimes.js
@@ -1,1 +1,25 @@
 /* eslint-disable no-unused-vars */
+function runNumTimes(callback,numTimes){
+    for(let i=0 ; i<numTimes;i++){
+         callback();
+   }
+}
+
+let myString = '';
+runNumTimes(() => {
+    myString += 'hello';
+    return myString;
+  }, 3
+);
+
+console.log(myString);
+  
+let myArray = [];
+let count = 1;
+runNumTimes(() => {
+    myArray.push(count);
+    count++;
+  }, 5);
+
+  console.log(myArray);
+  
