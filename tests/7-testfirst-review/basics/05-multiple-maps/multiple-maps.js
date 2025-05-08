133d220a062564e3d5da8a1bb836897c4ebfbab5
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-08 14:17:04 +0545

    test7-basic05: multiple-maps

--- tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
+++ tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function multipleMaps(values, maps) {
+  let result = [];
+  result = values.map((value) => {
+    for (const map of maps) {
+      if (value in map) {
+        return map[value];
+      }
+    }
+  });
+  return result;
+}
