7d9151b2a83e95f37a5ead2906a5cc9c92d3b46b
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-10 07:17:28 +0545

    test7-classes01: array-prototype

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,29 @@
 /* eslint-disable no-unused-vars */
+Array.prototype.maap = function (callback) {
+  const result = [];
+  this.forEach((item, index, array) => {
+    result.push(callback(item, index, array));
+  });
+  return result;
+};
+
+Array.prototype.fiilter = function (callback) {
+  const result = [];
+  this.forEach((item, index, array) => {
+    if (callback(item, index, array)) {
+      result.push(item);
+    }
+  });
+  return result;
+};
+
+Array.prototype.reeduce = function (callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : this[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+  this.slice(startIndex).forEach((item, index) => {
+    const actualIndex = startIndex + index;
+    accumulator = callback(accumulator, item, actualIndex, this);
+  });
+
+  return accumulator;
+};
