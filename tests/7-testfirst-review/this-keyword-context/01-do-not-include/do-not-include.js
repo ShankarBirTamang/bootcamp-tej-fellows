48fbdcee8ebcc138d6371a560aa37890e6a94a4b
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-12 18:58:09 +0545

    test7-this01: do-not-include

--- tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
+++ tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+Array.prototype.doNotInclude = function (indices) {
+  let indicesToExclude;
+  if (Array.isArray(indices)) {
+    indicesToExclude = indices;
+  } else {
+    indicesToExclude = [indices]; // convert to array
+  }
+  return this.filter((_, index) => !indicesToExclude.includes(index));
+};
