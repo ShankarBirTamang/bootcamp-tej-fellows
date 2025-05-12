0d21de5d8eb4041b2fb69de2fac5d4c0ac06c85a
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-12 17:44:49 +0545

    test7-closure01: sometimes

--- tests/7-testfirst-review/closure/01-sometimes/sometimes.js
+++ tests/7-testfirst-review/closure/01-sometimes/sometimes.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function sometimes(callback) {
+  let count = 0;
+  return function (...args) {
+    count++;
+    if (count <= 3) {
+      return callback.apply(this, args);
+    } else if (count % 2 === 0) {
+      return "I do not know!";
+    } else {
+      return callback.apply(this, args);
+    }
+  };
+}
