5359f33c201ac4755df56243c6ecb3d7007d085e
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-13 11:51:45 +0545

    test8.01: alternate-and-twice

--- tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
+++ tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function alternate(func) {
+  let shouldCall = true;
+  return function () {
+    if (shouldCall) {
+      func();
+    }
+    shouldCall = !shouldCall; //toggle for next call
+  };
+}
+
+function twice(func) {
+  let callCount = 0;
+  return function () {
+    if (callCount < 2) {
+      callCount++;
+      return func();
+    } else {
+      return 0;
+    }
+  };
+}
