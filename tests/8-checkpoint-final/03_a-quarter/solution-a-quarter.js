783b1fe91b222500d2dddadbd20dee74d4b878a4
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-13 14:28:39 +0545

    test8.3: a-quarter

--- tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
+++ tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function aQuarter(func) {
+  let callCount = 0;
+  return function (...args) {
+    callCount++;
+    if (callCount % 4 === 0) {
+      return func(...args);
+    } else {
+      return "something went wrong :/";
+    }
+  };
+}
