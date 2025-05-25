901ca65274d15bfb83e6b1cdd9d4fcf15cf33d97
Sankar Bir Tamang <sankar.jt19@gmail.com>
2025-05-25 16:34:35 +0545

    test 10: fibonacci function with recursive approach

--- tests/10-testfirst-recursion/recursion.js
+++ tests/10-testfirst-recursion/recursion.js
@@ -35,7 +35,10 @@ function recSmallestInt(array) {
   return firstChar < smallestOfRest ? firstChar : smallestOfRest;
 }
 
-function fib() {}
+function fib(n) {
+  if (n === 0 || n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 
 function stringify() {}
 
