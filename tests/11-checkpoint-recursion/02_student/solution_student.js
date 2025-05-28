8b4f89ce188ef01ba72afab318fd840c54af5241
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-28 21:53:03 +0545

    `Added Student class with constructor, addGrade method, and getGradeAverage method.`

--- tests/11-checkpoint-recursion/02_student/solution_student.js
+++ tests/11-checkpoint-recursion/02_student/solution_student.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+class Student {
+  constructor(name, email) {
+    this.name = name.slice(0, 1).toUpperCase() + name.slice(1);
+    this.email = email;
+    this.grades = [];
+  }
+  addGrade(grade) {
+    this.grades.push(grade);
+    return this; //this is for chaining
+  }
+
+  getGradeAverage() {
+    if (this.grades.length === 0) return 0;
+    let sum = this.grades.reduce((sum, currentValue) => currentValue + sum, 0);
+    return sum / this.grades.length;
+  }
+}
