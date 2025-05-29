52f820a21d37eb4d861c64cb246f8350b42b2f92
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-29 21:28:05 +0545

    pixelate

---
+++ tests/13-pixelate/script.js
@@ -0,0 +1,26 @@
+// Your code here
+/*
+- adding a new element to html page
+    - document.createElement
+    - parentElement.appendChild(newElement)
+- adding eventlistener to 'select' element
+    - what event should we listen to?
+- dragging to paint
+    - mousedown: addEventListener
+    - mouseover: do the action
+    - mouseup: removeEventListener
+*/
+let mySelect = document.getElementsByTagName('select')[0]
+let myFirstOption = document.createElement('option')
+myFirstOption.innerText = 'blueButton'
+myFirstOption.value = 'blue'
+mySelect.appendChild(myFirstOption)
+
+let mySecondOption = document.createElement('option')
+mySecondOption.innerText = 'redButton'
+mySecondOption.value = 'red'
+mySelect.appendChild(mySecondOption)
+
+mySelect.addEventListener("change", function(sharmila){
+    console.dir(sharmila.target.value)
+})
