68e51a44e4d3ffe875cb2eb9cd697650677f00af
ShankarBirTamang <sankar.jt19@gmail.com>
2025-05-29 14:10:10 +0545

    `Added event listener for game area click and implemented mole appearance and scoring logic.`

--- tests/12-whack-a-mole/index.js
+++ tests/12-whack-a-mole/index.js
@@ -1,1 +1,29 @@
 let score = 0;
+const scoreDisplay = document.getElementById("score");
+
+const gameArea = document.querySelector("#whack-a-mole");
+
+// gameArea.addEventListener("click", (clickEvent) => {
+//   if (clickEvent.target.tagName === "DIV") {
+//     const hole = clickEvent.target;
+//     hole.classList.toggle("mole");
+//     console.log("hole here");
+//   }
+// });
+
+const holes = document.querySelectorAll(".hole");
+
+setInterval(function () {
+  const randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle("mole");
+}, 300);
+
+gameArea.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.matches(".mole")) {
+    const hole = clickEvent.target;
+    hole.classList.remove("mole");
+    console.log("We hit a mole!");
+    score++;
+    scoreDisplay.innerHTML = score;
+  }
+});
