0feec2cb5973c5d3f421cf5a1376dfa3841d1109
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-01 11:08:03 +0545

    `Added livingNeighbors and conwayRule methods to GameOfLife class`

--- tests/14-game-of-life-main/GameOfLife.js
+++ tests/14-game-of-life-main/GameOfLife.js
@@ -46,23 +46,41 @@ class GameOfLife {
 
   livingNeighbors(row, col) {
     // TODO: Return the count of living neighbors.
+    return (
+      //Above Row
+      this.getCell(row - 1, col - 1) +
+      this.getCell(row - 1, col) +
+      this.getCell(row - 1, col + 1) +
+      //left & right
+      this.getCell(row, col - 1) +
+      this.getCell(row, col + 1) +
+      //Below Row
+      this.getCell(row + 1, col - 1) +
+      this.getCell(row + 1, col) +
+      this.getCell(row + 1, col + 1)
+    );
   }
 
   /**
    * Given the present board, apply the rules to generate a new board
    */
+  conwayRule(cell, livingNeighbors) {
+    return cell === 1
+      ? livingNeighbors === 2 || livingNeighbors === 3
+        ? 1
+        : 0
+      : livingNeighbors === 3
+      ? 1
+      : 0;
+  }
 
   tick() {
     const newBoard = this.makeBoard();
-    // TODO: Here is where you want to loop through all the cells
-    // on the existing board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the new board
-    // (the next iteration of the game)
-    //
-    // You need to:
-    // 1. Count alive neighbors for all cells
-    // 2. Set the next state of all cells in newBoard,
-    // based on their current alive neighbors
+
+    this.forEachCell((row, col, cell) => {
+      const neighbors = this.livingNeighbors(row, col);
+      newBoard[row][col] = this.conwayRule(cell, neighbors);
+    });
     this.board = newBoard;
   }
 }
