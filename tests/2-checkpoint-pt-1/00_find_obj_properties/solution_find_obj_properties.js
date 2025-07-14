3a7f872c3189ae4722b42320016dc6b48135a74d
Sankar Bir Tamang <85660085+ShankarBirTamang@users.noreply.github.com>
2025-07-14 19:43:45 +0545

    Update solution_find_obj_properties.js

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,23 +1,4 @@
-/* eslint-disable no-unused-vars, no-prototype-builtins */
 
-// The .hasOwnProperty() method is a built-in JavaScript method used to
-// check whether an object has a specific property as its own property,
-// not inherited from its prototype.
-
-const rectanglePrototype = {
-  getArea: function () {
-    return this.height * this.width;
-  },
-};
-function rectangle(color, height, width) {
-  const rectangleInstance = Object.create(rectanglePrototype);
-
-  rectangleInstance.color = color;
-  rectangleInstance.height = height;
-  rectangleInstance.width = width;
-
-  return rectangleInstance;
-}
 function findObjPropsHasOwn(obj) {
   keys = [];
   for (key in obj) {
@@ -28,19 +9,10 @@ function findObjPropsHasOwn(obj) {
   return keys.join(", ");
 }
 
-const greenRectangle = { color: "green" };
-console.log(findObjPropsHasOwn(greenRectangle));
-const yelloRectangle = { color: "yellow", height: 8, width: 5 };
-console.log(findObjPropsHasOwn(yelloRectangle));
 
-const blueRectangle = rectangle("blue", 5, 3);
-console.log(findObjPropsHasOwn(blueRectangle));
 
 // ---- 2nd part
 
 function findObjKeys(obj) {
   return Object.keys(obj).join(", ");
 }
-console.log(findObjKeys(greenRectangle));
-console.log(findObjKeys(yelloRectangle));
-console.log(findObjKeys(blueRectangle));
