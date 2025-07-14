8d594cb9483d5563aebe6d8e30185c20d71e9cf4
Sankar Bir Tamang <85660085+ShankarBirTamang@users.noreply.github.com>
2025-07-14 19:44:20 +0545

    Update solution_multiplication_table.js

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -11,12 +11,3 @@ function multiplicationTable(rows, cols) {
   return table;
 }
 
-const smallTable = multiplicationTable(3, 0);
-const mediumTable = multiplicationTable(6, 0);
-const largeTable = multiplicationTable(9, 0);
-
-console.log(smallTable);
-console.log(mediumTable);
-console.log(largeTable);
-
-console.log(multiplicationTable(2, 3));
