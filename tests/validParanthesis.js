2496ad21be3de66e4beee89bdc1ab95c88f83208
ShankarBirTamang <sankar.jt19@gmail.com>
2025-07-21 08:49:37 +0545

    valid

---
+++ tests/validParanthesis.js
@@ -0,0 +1,25 @@
+var isValid = function (s) {
+  debugger;
+  let stack = [];
+  let map = {
+    "]": "[",
+    "}": "{",
+    ")": "(",
+  };
+  for (let char of s) {
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      if (stack.pop() !== map[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("()")); // true
+console.log(isValid("()[]{}")); // true
+console.log(isValid("(]")); // false
+console.log(isValid("([)]")); // false
+console.log(isValid("{[]}")); // true
